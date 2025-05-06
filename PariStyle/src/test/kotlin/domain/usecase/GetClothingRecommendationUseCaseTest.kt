d45f864c3fc665a4316b17f2b7e2f2f13a10d276
Karim Roshdy <karimroshdy911@gmail.com>
2025-05-06 18:20:42 +0300

    write test case for getClothingRecommendationUseCase

--- PariStyle/src/test/kotlin/domain/usecase/GetClothingRecommendationUseCaseTest.kt
+++ PariStyle/src/test/kotlin/domain/usecase/GetClothingRecommendationUseCaseTest.kt
@@ -1,15 +1,17 @@
 package domain.usecase
 
-import com.google.common.truth.Truth.assertThat
+
 import domain.model.entity.Weather
 import io.mockk.coEvery
 import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
+import org.example.domain.model.entity.ClothingType
 import org.example.domain.model.entity.WeatherCondition
 import org.example.domain.usecase.GetClothingRecommendationUseCase
 import org.example.domain.usecase.GetWeatherUseCase
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
 class GetClothingRecommendationUseCaseTest {
     private val getWeatherUseCase: GetWeatherUseCase = mockk()
@@ -21,13 +23,31 @@ class GetClothingRecommendationUseCaseTest {
     }
 
     @Test
-    fun `should return correct clothing recommendation based on weather`() = runTest {
-        try {
-            val expectedWeather = Weather(53.0, "°C",WeatherCondition.CLEAR_SKY)
-            coEvery { getWeatherUseCase.getWeather() } returns expectedWeather
-            getClothingRecommendationUseCase.getClothingRecommendation()
-        } catch (e: Throwable) {
-            assertThat(e).isInstanceOf(NotImplementedError::class.java)
-        }
+    fun `returns Jacket for cold snowy weather`() = runTest {
+        val weather = Weather(0.0,"c",WeatherCondition.SNOW_GRAINS)
+        coEvery{getWeatherUseCase.getWeather()} returns  weather
+
+        val result = getClothingRecommendationUseCase.getClothingRecommendation()
+
+        assertEquals(ClothingType.HEAVY_JACKET, result.type)
+    }
+    @Test
+    fun `returns Sweater for mild sunny weather`() = runTest {
+        val weather= Weather(15.0,"c",WeatherCondition.MAINLY_CLEAR)
+        coEvery { getWeatherUseCase.getWeather() } returns weather
+
+        val result= getClothingRecommendationUseCase.getClothingRecommendation()
+
+        assertEquals(ClothingType.SWEATER,result.type)
+    }
+
+    @Test
+    fun `returns T-Shirt for warm sunny weather`() = runTest {
+        val weather = Weather(30.0,"c", WeatherCondition.MAINLY_CLEAR)
+        coEvery { getWeatherUseCase.getWeather() } returns weather
+
+        val result = getClothingRecommendationUseCase.getClothingRecommendation()
+
+        assertEquals(ClothingType.T_SHIRT,result.type)
     }
 }
